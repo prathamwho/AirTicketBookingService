13d36ef7635e6de0032366f21e5c28347a74e5f2
prathamwho <praj79074@gmail.com>
2025-10-01 17:33:19 +0530

    Booking Service basics and server setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+const setupAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+    })
+}
+
+setupAndStartServer();
